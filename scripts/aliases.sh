8dcffc0bac9f259ecaaa4ae91871745023e19359
Mhar Andal <mhar.andal@gamut.com>
2018-03-21 14:56:54 -0500

    Add git aliases in scripts

---
+++ scripts/aliases.sh
@@ -0,0 +1,16 @@
+# general
+alias ls='ls -G'
+
+# git aliases
+alias ga='git add'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gd='git diff'
+alias gs='gst'
+alias gst='git status'
+alias ggpull='git pull'
+alias ggpush='git push'
+
+# ruby
+alias be='bundle exec'
